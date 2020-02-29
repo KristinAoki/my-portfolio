6cdea4f5277e5ab32eb47f530d115174077a6c13
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2020-02-29 10:27:58 -0500

    Reset to previous version

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,9 +26,8 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String message = "Hello Kristin!";
     
     response.setContentType("text/html;");
-    response.getWriter().println(message);
+    response.getWriter().println("Hello Kristin");
   }
 }
