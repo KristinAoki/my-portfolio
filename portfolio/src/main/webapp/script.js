11f8c9cddd8c864048a8ef9eb2afeb02aeda3212
Kristin Aoki <kalexaoki@gmail.com>
2020-02-29 10:32:21 -0500

    Delete unused functions

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,20 +13,8 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a message from data servlet to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
 function getData() {
     console.log('Fetching data.');
     const responsePromise = fetch('/random-quote');
